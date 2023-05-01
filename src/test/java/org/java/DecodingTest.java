c90005abe072a6eeb27fa93a0659ea9532db4eb1
Muli <arthurkaku@gmail.com>
2023-05-01 16:32:32 +0300

    Readme added

--- src/test/java/org/java/DecodingTest.java
+++ src/test/java/org/java/DecodingTest.java
@@ -1,7 +1,16 @@
 package org.java;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DecodingTest {
 
+    @Test
+    @DisplayName("Testing Decoding Program")
+    void decode() {
+        assertEquals("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", Decoding.decode("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD", 23));
+        assertEquals("B@#", Decoding.decode("A@#", -1));
+    }
 }
